e2a6bd8f2fbe13689fe9c92200b25250083d533a
Tarnvir Grewal <t-tagrew@microsoft.com>
2019-10-10 13:42:39 +1100

    buttons working but have delay

--- Device/GetStarted.ino
+++ Device/GetStarted.ino
@@ -4,20 +4,19 @@
 #include "AZ3166WiFi.h"
 #include "AzureIotHub.h"
 #include "DevKitMQTTClient.h"
+#include "OledDisplay.h"
+#include "Sensor.h"
 
 #include "config.h"
 #include "utility.h"
 #include "SystemTickCounter.h"
+#include "Arduino.h"
 //#define RGB_LED_BRIGHTNESS  32
 //static RGB_LED rgbLed;
 static bool hasWifi = false;
-int messageCount = 1;
-int sentMessageCount = 0;
-static bool messageSending = true;
-static uint64_t send_interval_ms;
-
-static float temperature;
-static float humidity;
+int btnAState;
+int btnBState;
+int app_status = 0;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Utilities
@@ -38,107 +37,51 @@ static void InitWifi()
     Screen.print(1, "No Wi-Fi\r\n ");
   }
 }
-static void adventureButton()
+
+static void chooseRoad1()
 {
+      
+  Screen.clean();
+  Screen.print(1, "2 paths infront");    
+  Screen.print(2,"A: Go Left");
+  Screen.print(3,"B: Go Right");
+  if (digitalRead(USER_BUTTON_A) == LOW)
+  {
+    //chooseRoad1();   
+    //app_status =2;
+     
+  }
+  else if (digitalRead(USER_BUTTON_B) == LOW)
+  {
+    //chooseRoad1();  
+    //app_status =3;
+  }
+
+
+}
+
+static void startAdventure()
+{
+  blinkLED();
+  Screen.print(1, "Welcome to ");
+  Screen.print(2, "Floop! Choose ");
+  Screen.print(3,"your path wisely");
+  
+
   if (digitalRead(USER_BUTTON_A) == LOW)
     {
-      // If selected option A
-
-      // Set the RGB LED to green
-      //rgbLed.setColor(0, RGB_LED_BRIGHTNESS, 0);
-
-      Screen.clean();
-      Screen.print(1, "2 paths infront");    
-      Screen.print(2,"A: Go Left");
-      Screen.print(3,"B: Go Right");
+      //chooseRoad1();   
+      app_status =1;
       
     }
     else if (digitalRead(USER_BUTTON_B) == LOW)
     {
-      
-      // Set the RGB LED to green
-      //rgbLed.setColor(0, RGB_LED_BRIGHTNESS, 0);
-
-      Screen.clean();
-      Screen.print(1, "2 paths infront");    
-      Screen.print(2,"A: Go Left");
-      Screen.print(3,"B: Go Right");
+      //chooseRoad1();  
+      app_status =1;
     }
 }
-static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
-{
 
-  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
-  {
-    blinkSendConfirmation();
-    sentMessageCount++;
-  }
-  // add code here 
-  Screen.print(1, "Welcome to ");
-  Screen.print(2, "Floop! Choose ");
-  Screen.print(3,"your path wisely");
-  adventureButton();
 
-  /*
-  char line1[20];
-  sprintf(line1, "Count: %d/%d",sentMessageCount, messageCount); 
-  Screen.print(2, line1);
-
-  char line2[20];
-  sprintf(line2, "T:%.2f H:%.2f", temperature, humidity);
-  Screen.print(3, line2);
-  messageCount++;
-
-  */
-}
-
-static void MessageCallback(const char* payLoad, int size)
-{
-  blinkLED();
-  Screen.print(1, payLoad, true);
-}
-
-static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsigned char *payLoad, int size)
-{
-  char *temp = (char *)malloc(size + 1);
-  if (temp == NULL)
-  {
-    return;
-  }
-  memcpy(temp, payLoad, size);
-  temp[size] = '\0';
-  parseTwinMessage(updateState, temp);
-  free(temp);
-}
-
-static int  DeviceMethodCallback(const char *methodName, const unsigned char *payload, int size, unsigned char **response, int *response_size)
-{
-  LogInfo("Try to invoke method %s", methodName);
-  const char *responseMessage = "\"Successfully invoke device method\"";
-  int result = 200;
-
-  if (strcmp(methodName, "start") == 0)
-  {
-    LogInfo("Start sending temperature and humidity data");
-    messageSending = true;
-  }
-  else if (strcmp(methodName, "stop") == 0)
-  {
-    LogInfo("Stop sending temperature and humidity data");
-    messageSending = false;
-  }
-  else
-  {
-    LogInfo("No method %s found", methodName);
-    responseMessage = "\"No method found\"";
-    result = 404;
-  }
-
-  *response_size = strlen(responseMessage) + 1;
-  *response = (unsigned char *)strdup(responseMessage);
-
-  return result;
-}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Arduino sketch
@@ -160,63 +103,31 @@ void setup()
     return;
   }
 
-  LogTrace("HappyPathSetup", NULL);
+  // Initialize button
+  pinMode(USER_BUTTON_A, INPUT);
+  pinMode(USER_BUTTON_B, INPUT);
+  btnAState = digitalRead(USER_BUTTON_A);
+  btnBState = digitalRead(USER_BUTTON_B);
+  
 
-  Screen.print(3, " > Sensors");
-  SensorInit();
 
-  Screen.print(3, " > IoT Hub");
-  DevKitMQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "DevKit-GetStarted");
-  DevKitMQTTClient_Init(true);
-
-  DevKitMQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
-  DevKitMQTTClient_SetMessageCallback(MessageCallback);
-  DevKitMQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
-  DevKitMQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
-
-  send_interval_ms = SystemTickCounterRead();
 }
 
 void loop()
 {
   if (hasWifi)
   {
-
     switch(app_status)
     {
       case 0:
-        DoIdle();
+        startAdventure();
         break;
-      
       case 1:
-        DoShake();
-        break;
-        
-      case 2:
-        DoWork();
+        chooseRoad1();
         break;
     }
+    
   }
-
-  /*
-    if (messageSending && 
-        (int)(SystemTickCounterRead() - send_interval_ms) >= getInterval())
-    {
-      // Send teperature data
-      char messagePayload[MESSAGE_MAX_LEN];
-
-      bool temperatureAlert = readMessage(messageCount, messagePayload, &temperature, &humidity);
-      EVENT_INSTANCE* message = DevKitMQTTClient_Event_Generate(messagePayload, MESSAGE);
-      DevKitMQTTClient_Event_AddProp(message, "temperatureAlert", temperatureAlert ? "true" : "false");
-      DevKitMQTTClient_SendEventInstance(message);
-      
-      send_interval_ms = SystemTickCounterRead();
-    }
-    else
-    {
-      DevKitMQTTClient_Check();
-    }
-  */
-  }
+   
   delay(1000);
 }
